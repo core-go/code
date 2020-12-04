ab253cf408c721d2f75aad9d6aa328b2e0f0878a
kietnguyentuan69 <kietnguyentuan69@gmail.com>
2020-12-04 14:22:42 +0700

    - Refactor code

--- code.go
+++ code.go
@@ -50,16 +50,34 @@ type DynamicSqlCodeLoader struct {
 	DB             *sql.DB
 	Query          string
 	ParameterCount int
-	HandleDriver   bool
 	Driver         string
 }
+
 func NewDefaultDynamicSqlCodeLoader(db *sql.DB, query string) *DynamicSqlCodeLoader {
 	driver := GetDriver(db)
-	return &DynamicSqlCodeLoader{DB: db, Query: query, ParameterCount: 0, HandleDriver: true, Driver: driver}
+	return &DynamicSqlCodeLoader{DB: db, Query: query, ParameterCount: 0, Driver: driver}
+	return NewDynamicSqlCodeLoader(db, query, 0, true)
 }
 func NewDynamicSqlCodeLoader(db *sql.DB, query string, parameterCount int, handleDriver bool) *DynamicSqlCodeLoader {
 	driver := GetDriver(db)
-	return &DynamicSqlCodeLoader{DB: db, Query: query, ParameterCount: parameterCount, HandleDriver: handleDriver, Driver: driver}
+	if parameterCount <= 0 {
+		parameterCount = 1
+	}
+	if handleDriver {
+		if driver == DriverOracle || driver == DriverPostgres {
+			var x string
+			if driver == DriverOracle {
+				x = ":val"
+			} else {
+				x = "$"
+			}
+			for i := 0; i < parameterCount; i++ {
+				count := i + 1
+				query = strings.Replace(query, "?", x+strconv.Itoa(count), 1)
+			}
+		}
+	}
+	return &DynamicSqlCodeLoader{DB: db, Query: query, ParameterCount: parameterCount, Driver: driver}
 }
 func (l DynamicSqlCodeLoader) Load(ctx context.Context, master string) ([]CodeModel, error) {
 	models := make([]CodeModel, 0)
@@ -71,20 +89,6 @@ func (l DynamicSqlCodeLoader) Load(ctx context.Context, master string) ([]CodeMo
 		}
 	}
 	driver := l.Driver
-	if l.HandleDriver {
-		if driver == DriverOracle || driver == DriverPostgres {
-			var x string
-			if driver == DriverOracle {
-				x = ":val"
-			} else {
-				x = "$"
-			}
-			for i := 0; i < len(params); i++ {
-				count := i + 1
-				l.Query = strings.Replace(l.Query, "?", x + strconv.Itoa(count), 1)
-			}
-		}
-	}
 	rows, er1 := l.DB.Query(l.Query, params...)
 	if er1 != nil {
 		return models, er1
@@ -167,7 +171,7 @@ func (l SqlCodeLoader) Load(ctx context.Context, master string) ([]CodeModel, er
 			p2 = fmt.Sprintf("%s = $%d", c.Status, i)
 		} else if l.Driver == DriverOracle {
 			p1 = fmt.Sprintf("%s = :val%d", c.Status, i)
-		}else {
+		} else {
 			p2 = fmt.Sprintf("%s = ?", c.Status)
 		}
 		values = append(values, c.Active)
@@ -190,9 +194,9 @@ func (l SqlCodeLoader) Load(ctx context.Context, master string) ([]CodeModel, er
 		}
 	}
 	if len(sql2) > 0 {
-		if l.Driver == DriverOracle || l.Driver  == DriverPostgres {
+		if l.Driver == DriverOracle || l.Driver == DriverPostgres {
 			var x string
-			if l.Driver  == DriverOracle {
+			if l.Driver == DriverOracle {
 				x = ":val"
 			} else {
 				x = "$"
