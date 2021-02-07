25a6658095b7f5b7bd3ec2c7c200d69067dec1e8
Duc Nguyen <minhduc140583@gmail.com>
2021-02-07 20:30:54 +0700

    Refactor writeLog

--- handler.go
+++ handler.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-const InternalServerError = "Internal Server Error"
+const internalServerError = "Internal Server Error"
 
 type CodeHandler struct {
 	Loader         CodeLoader
@@ -16,20 +16,20 @@ type CodeHandler struct {
 	Action         string
 	RequiredMaster bool
 	LogError       func(context.Context, string)
-	LogWriter      LogWriter
+	WriteLog       func(ctx context.Context, resource string, action string, success bool, desc string) error
 }
 
-func NewDefaultCodeHandler(loader CodeLoader, resource string, action string, logError func(context.Context, string), logWriter LogWriter) *CodeHandler {
-	return NewCodeHandler(loader, resource, action, true, logError, logWriter)
+func NewDefaultCodeHandler(loader CodeLoader, resource string, action string, logError func(context.Context, string), writeLog func(context.Context, string, string, bool, string) error) *CodeHandler {
+	return NewCodeHandler(loader, resource, action, true, logError, writeLog)
 }
-func NewCodeHandler(loader CodeLoader, resource string, action string, requiredMaster bool, logError func(context.Context, string), logWriter LogWriter) *CodeHandler {
+func NewCodeHandler(loader CodeLoader, resource string, action string, requiredMaster bool, logError func(context.Context, string), writeLog func(context.Context, string, string, bool, string) error) *CodeHandler {
 	if len(resource) == 0 {
 		resource = "code"
 	}
 	if len(action) == 0 {
 		action = "load"
 	}
-	h := CodeHandler{Loader: loader, Resource: resource, Action: action, RequiredMaster: requiredMaster, LogWriter: logWriter, LogError: logError}
+	h := CodeHandler{Loader: loader, Resource: resource, Action: action, RequiredMaster: requiredMaster, WriteLog: writeLog, LogError: logError}
 	return &h
 }
 func (c *CodeHandler) Load(w http.ResponseWriter, r *http.Request) {
@@ -51,35 +51,31 @@ func (c *CodeHandler) Load(w http.ResponseWriter, r *http.Request) {
 	}
 	result, er4 := c.Loader.Load(r.Context(), code)
 	if er4 != nil {
-		respondError(w, r, http.StatusInternalServerError, InternalServerError, c.LogError, c.Resource, c.Action, er4, c.LogWriter)
+		respondError(w, r, http.StatusInternalServerError, internalServerError, c.LogError, c.Resource, c.Action, er4, c.WriteLog)
 	} else {
-		succeed(w, r, http.StatusOK, result, c.LogWriter, c.Resource, c.Action)
+		succeed(w, r, http.StatusOK, result, c.WriteLog, c.Resource, c.Action)
 	}
-}
-
-type LogWriter interface {
-	Write(ctx context.Context, resource string, action string, success bool, desc string) error
 }
 
 func respondString(w http.ResponseWriter, r *http.Request, code int, result string) {
 	w.WriteHeader(code)
 	w.Write([]byte(result))
 }
-func respond(w http.ResponseWriter, r *http.Request, code int, result interface{}, logWriter LogWriter, resource string, action string, success bool, desc string) {
+func respond(w http.ResponseWriter, r *http.Request, code int, result interface{}, writeLog func(context.Context, string, string, bool, string) error, resource string, action string, success bool, desc string) {
 	response, _ := json.Marshal(result)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
-	if logWriter != nil {
-		logWriter.Write(r.Context(), resource, action, success, desc)
+	if writeLog != nil {
+		writeLog(r.Context(), resource, action, success, desc)
 	}
 }
-func respondError(w http.ResponseWriter, r *http.Request, code int, result interface{}, logError func(context.Context, string), resource string, action string, err error, logWriter LogWriter) {
+func respondError(w http.ResponseWriter, r *http.Request, code int, result interface{}, logError func(context.Context, string), resource string, action string, err error, writeLog func(context.Context, string, string, bool, string) error) {
 	if logError != nil {
 		logError(r.Context(), err.Error())
 	}
-	respond(w, r, code, result, logWriter, resource, action, false, err.Error())
+	respond(w, r, code, result, writeLog, resource, action, false, err.Error())
 }
-func succeed(w http.ResponseWriter, r *http.Request, code int, result interface{}, logWriter LogWriter, resource string, action string) {
-	respond(w, r, code, result, logWriter, resource, action, true, "")
+func succeed(w http.ResponseWriter, r *http.Request, code int, result interface{}, writeLog func(context.Context, string, string, bool, string) error, resource string, action string) {
+	respond(w, r, code, result, writeLog, resource, action, true, "")
 }
