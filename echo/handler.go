ba96c4b9dea2ce4419c6e14066d37ba5f60043c2
Duc Nguyen <minhduc140583@gmail.com>
2022-06-25 07:56:33 +0700

    Refactor code

--- echo/handler.go
+++ echo/handler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -100,6 +101,49 @@ func (h *Handler) Load(ctx echo.Context) error {
 				rs = append(rs, m)
 			}
 			return succeed(ctx, http.StatusOK, rs, h.Log, h.Resource, h.Action)
+		}
+	}
+}
+
+type QueryHandler struct {
+	Load     func(ctx context.Context, key string, max int64) ([]string, error)
+	LogError func(context.Context, string, ...map[string]interface{})
+	Keyword  string
+	Max      string
+}
+
+func NewQueryHandler(load func(ctx context.Context, key string, max int64) ([]string, error), logError func(context.Context, string, ...map[string]interface{}), opts ...string) *QueryHandler {
+	keyword := "q"
+	if len(opts) > 0 && len(opts[0]) > 0 {
+		keyword = opts[0]
+	}
+	max := "max"
+	if len(opts) > 1 && len(opts[1]) > 0 {
+		max = opts[1]
+	}
+	return &QueryHandler{load, logError, keyword, max}
+}
+func (h *QueryHandler) Query(ctx echo.Context) error {
+	ps := ctx.Request().URL.Query()
+	keyword := ps.Get(h.Keyword)
+	if len(keyword) == 0 {
+		vs := make([]string, 0)
+		return ctx.JSON(http.StatusOK, vs)
+	} else {
+		max := ps.Get(h.Max)
+		i, err := strconv.ParseInt(max, 10, 64)
+		if err != nil {
+			i = 20
+		}
+		if i < 0 {
+			i = 20
+		}
+		vs, err := h.Load(ctx.Request().Context(), keyword, i)
+		if err != nil {
+			h.LogError(ctx.Request().Context(), err.Error())
+			return ctx.String(http.StatusInternalServerError, internalServerError)
+		} else {
+			return ctx.JSON(http.StatusOK, vs)
 		}
 	}
 }
--- gin/handler.go
+++ gin/handler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -100,6 +101,49 @@ func (h *Handler) Load(ctx *gin.Context) {
 				rs = append(rs, m)
 			}
 			succeed(ctx, http.StatusOK, rs, h.Log, h.Resource, h.Action)
+		}
+	}
+}
+
+type QueryHandler struct {
+	Load     func(ctx context.Context, key string, max int64) ([]string, error)
+	LogError func(context.Context, string, ...map[string]interface{})
+	Keyword  string
+	Max      string
+}
+
+func NewQueryHandler(load func(ctx context.Context, key string, max int64) ([]string, error), logError func(context.Context, string, ...map[string]interface{}), opts ...string) *QueryHandler {
+	keyword := "q"
+	if len(opts) > 0 && len(opts[0]) > 0 {
+		keyword = opts[0]
+	}
+	max := "max"
+	if len(opts) > 1 && len(opts[1]) > 0 {
+		max = opts[1]
+	}
+	return &QueryHandler{load, logError, keyword, max}
+}
+func (h *QueryHandler) Query(ctx *gin.Context) {
+	ps := ctx.Request.URL.Query()
+	keyword := ps.Get(h.Keyword)
+	if len(keyword) == 0 {
+		vs := make([]string, 0)
+		ctx.JSON(http.StatusOK, vs)
+	} else {
+		max := ps.Get(h.Max)
+		i, err := strconv.ParseInt(max, 10, 64)
+		if err != nil {
+			i = 20
+		}
+		if i < 0 {
+			i = 20
+		}
+		vs, err := h.Load(ctx.Request.Context(), keyword, i)
+		if err != nil {
+			h.LogError(ctx.Request.Context(), err.Error())
+			ctx.String(http.StatusInternalServerError, internalServerError)
+		} else {
+			ctx.JSON(http.StatusOK, vs)
 		}
 	}
 }
